c55718b7370c722fec154194d8b87829eb7371c1
Bandipo <bandipotaiye@gmail.com>
2023-06-16 05:25:10 -0700

    add c program that prints a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints a message to the console.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
