6359d66b3b30beee4bb9bbb10b8a392b882bed28
Bandipo <bandipotaiye@gmail.com>
2023-06-16 05:20:10 -0700

    fix betty errors in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints a message to the console.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
