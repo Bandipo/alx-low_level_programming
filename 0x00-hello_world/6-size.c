8029d97f6fa1516ffd7c7e71676e56d686d5b334
Bandipo <bandipotaiye@gmail.com>
2023-06-16 14:30:17 -0700

    add a c file to print integer data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints the size of various data types.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	printf("Size of a char: %zu\n", sizeof(char));
+	printf("Size of an int: %zu\n", sizeof(int));
+	printf("Size of a long int: %zu\n", sizeof(long int));
+	printf("Size of a long long int: %zu\n", sizeof(long long int));
+	printf("Size of a float: %zu\n", sizeof(float));
+
+	return (0);
+}
