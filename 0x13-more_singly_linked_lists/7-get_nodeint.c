a431cc153ec759a57321ee0d3e4955fb092e7aa5
Bandipo <bandipotaiye@gmail.com>
2023-09-27 01:53:22 -0700

    fix code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  *
  * Return: Pointer to the node at the specified index, or NULL if not found.
  */
-listint_t *get_node_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 	listint_t *current = head;
